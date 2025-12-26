b672442bfa18c1aa67f7d73fb1e476c948bb4112
Eslam-Elngar <eslamelngar555@gmail.com>
2025-12-26 07:54:54 +0200

    responsive fixes

--- src/Components/NavBar.jsx
+++ src/Components/NavBar.jsx
@@ -1,7 +1,7 @@
 import { NavLink, Link } from "react-router-dom";
 import { useContext } from "react";
 import { CartContext } from "../Context/CartContext";
-import { Cart3 } from "react-bootstrap-icons";
+import { Cart3, List } from "react-bootstrap-icons";
 import "./NavBar.css";
 
 export default function NavBar() {
@@ -20,9 +20,35 @@ export default function NavBar() {
           Fayrouz
         </NavLink>
 
+        <div className="d-flex align-items-center gap-2 gap-lg-3">
+          <Link
+            to="/cart"
+            className="position-relative text-decoration-none"
+          >
+            <Cart3 className="cart-icon" />
+            {totalItems > 0 && (
+              <span className="cart-badge badge rounded-pill bg-danger">
+                {totalItems}
+              </span>
+            )}
+          </Link>
+
+          <button
+            className="navbar-toggler border-0"
+            type="button"
+            data-bs-toggle="collapse"
+            data-bs-target="#mainNav"
+            aria-controls="mainNav"
+            aria-expanded="false"
+            aria-label="Toggle navigation"
+          >
+            <List size={28} color="green" />
+          </button>
+        </div>
+
         <div className="collapse navbar-collapse" id="mainNav">
           <ul className="navbar-nav ms-auto mb-2 mb-lg-0">
-            <li className="nav-item ms-4 fs-4">
+            <li className="nav-item ms-lg-4 fs-4">
               <NavLink
                 to="/"
                 className={({ isActive }) =>
@@ -32,7 +58,7 @@ export default function NavBar() {
                 الرئيسية
               </NavLink>
             </li>
-            <li className="nav-item ms-4 fs-4">
+            <li className="nav-item ms-lg-4 fs-4">
               <NavLink
                 to="/products"
                 className={({ isActive }) =>
@@ -42,7 +68,7 @@ export default function NavBar() {
                 المنتجات
               </NavLink>
             </li>
-            <li className="nav-item ms-4 fs-4">
+            <li className="nav-item ms-lg-4 fs-4">
               <NavLink
                 to="/contact"
                 className={({ isActive }) =>
@@ -54,20 +80,7 @@ export default function NavBar() {
             </li>
           </ul>
         </div>
-
-        
       </div>
-      <Link
-          to="/cart"
-          className="position-relative text-decoration-none me-5 "
-        >
-          <Cart3 className="cart-icon" />
-          {totalItems > 0 && (
-            <span className="cart-badge badge rounded-pill bg-danger">
-              {totalItems}
-            </span>
-          )}
-        </Link>
     </nav>
   );
 }
